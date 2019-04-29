a7d4a2b13e31b406e01bed8e761df543f9ce4005
eslan94 <esteban.calvopina@epn.edu.ec>
2019-04-29 18:52:50 -0500

    Distancia

---
+++ Distancia.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float dist1=70, dist2=150;
+	float dist3, res;
+	dist3= (dist2-dist1)/2;
+	res= dist1+dist3;
+	cout<<"Los amigos se encontraran en el kilomentro: "<<res;
+	return (0);
+}
