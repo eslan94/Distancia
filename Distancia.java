6ffff917d45f22301426463921caa038c7e8fe26
eslan94 <esteban.calvopina@epn.edu.ec>
2019-04-29 11:28:55 -0500

    Distancia

---
+++ Distancia.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+    float dist1=70, dist2=150;
+    float dist3, res;
+    dist3=(dist2-dist1)/2;
+    res=dist1+dist3;
+    System.out.println("Los amigos se encontraran en el kilometro: "+res);
+  }
+
+}
